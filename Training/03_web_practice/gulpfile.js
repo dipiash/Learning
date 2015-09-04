22b834829a6375d23add7eb3ece8f41fa1cbe6f4
dipiash <dipiash@users.noreply.github.com>
2015-09-04 12:26:48 +0300

    Added default files for 03_web_practice

---
+++ Training/03_web_practice/gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp'),
+    csso = require('gulp-csso'),
+    less = require('gulp-less'),
+    path = require('path'),
+    LessPluginCleanCSS = require('less-plugin-clean-css'),
+    cleancss = new LessPluginCleanCSS({ advanced: true }),
+    watch = require('gulp-watch'),
+    batch = require('gulp-batch');
+
+gulp.task('less', function () {
+  return gulp.src('less/*.less')
+    .pipe(less({ plugins: [cleancss] }))
+    .pipe(csso())
+    .pipe(gulp.dest('./css/'));
+});
+
+gulp.task('watch', function () {
+    watch('less/*.less', batch(function (events, done) {
+        gulp.start('less', done);
+    }));
+});
+
+gulp.task('default', ['less', 'watch'])
