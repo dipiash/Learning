81af3b4b45baeed3bd926d770e0815f52d452925
dipiash <dipiash@users.noreply.github.com>
2015-09-07 15:20:49 +0300

    Added info page< for only signed in users

--- Training/06_web_practice/devise_app/app/controllers/pages_controller.rb
+++ Training/06_web_practice/devise_app/app/controllers/pages_controller.rb
@@ -1,4 +1,11 @@
 class PagesController < ApplicationController
+
+  before_filter :authenticate_user!, except: [:index]
+
   def index
+  end
+
+  def info
+    @info = "Only signed in users!"
   end
 end
--- Training/06_web_practice/devise_app/config/routes.rb
+++ Training/06_web_practice/devise_app/config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
 
   root 'pages#index'
+
+  get 'info', to: 'pages#info', via: 'get'
 end
